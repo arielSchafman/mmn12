6cff468536a8d8138257e87e0c97a23ffbf4e774
ariel <ariel.schafman@gmail.com>
2019-12-31 12:24:41 +0200

    master

--- src/FoodItemTester.java
+++ src/FoodItemTester.java
@@ -1,12 +1,11 @@
-public class FoodItemTester
-{
+public class FoodItemTester {
     public static void main(String[] args) {
         System.out.println("********** Test FoodItem - Started **********");
         System.out.println("\n1. Testing Constructors and toString:");
-        Date d1=new Date(14,12,2019);
-        Date d2=new Date(21,12,2019);
+        Date d1 = new Date(14, 12, 2019);
+        Date d2 = new Date(21, 12, 2019);
         // test concstructor which takes 8 parameters
-        FoodItem fi1 = new FoodItem(new String("milk"),111111,6,d1,d2, 10, 25,5);
+        FoodItem fi1 = new FoodItem(new String("milk"), 111111, 6, d1, d2, 10, 25, 5);
         System.out.println("\tFoodItem 1:\n\t" + fi1);
         // test copy concstructor
         FoodItem fi2 = new FoodItem(fi1);
@@ -24,25 +23,25 @@ public class FoodItemTester
 
         // test setters   
         fi2.setQuantity(4);
-        Date d3=new Date(18,12,2019);
-        Date d4=new Date(28,12,2019);
+        Date d3 = new Date(18, 12, 2019);
+        Date d4 = new Date(28, 12, 2019);
         fi2.setProductionDate(d3);
         fi2.setExpiryDate(d4);
         fi2.setPrice(2);
         System.out.println("\tFoodItem 2:\n\t" + fi2);
 
         System.out.println("\n3. Testing comparison methods:");
-        FoodItem fi3 = new FoodItem(new String("milk"),111111,6,d1,d2, 10, 25,5);
+        FoodItem fi3 = new FoodItem(new String("milk"), 111111, 6, d1, d2, 10, 25, 5);
         System.out.println("\tFoodItem 1 and FoodItem 3 are equal: " + fi1.equals(fi3));
         System.out.println("\tFoodItem 1 is older than FoodItem 2: " + fi1.olderFoodItem(fi2));
-        System.out.println("\tFoodItem 1 is fresh at "+d3+" : " + fi1.isFresh(d3)); 
+        System.out.println("\tFoodItem 1 is fresh at " + d3 + " : " + fi1.isFresh(d3));
         System.out.println("\tFoodItem 1 is cheaper than FoodItem 2: " + fi1.isCheaper(fi2));
         System.out.println("\n4. Testing howManyItems method :");
-        int amount=10;
-        System.out.println("\t"+ amount+" sheqels can buy "+ fi1.howManyItems(amount)+" of FoodItem 1");
-        System.out.println("\t"+ amount+" sheqels can buy "+ fi2.howManyItems(amount)+" of FoodItem 2");
+        int amount = 10;
+        System.out.println("\t" + amount + " sheqels can buy " + fi1.howManyItems(amount) + " of FoodItem 1");
+        System.out.println("\t" + amount + " sheqels can buy " + fi2.howManyItems(amount) + " of FoodItem 2");
 
-        
+
         System.out.println("\n********** Test FoodItem - Finished **********\n");
     }
 }
